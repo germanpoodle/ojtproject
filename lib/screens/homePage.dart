00020071583e32380c67a37b32bc5b4d5a29a09b
DaVinci1230 <angelo.burgos.habaradas@gmail.com>
2024-06-13 16:16:08 +0800

    homepage color

--- lib/screens/homePage.dart
+++ lib/screens/homePage.dart
@@ -1,82 +1,75 @@
 import 'disbursement_check.dart';
+import 'transaction_history.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+class Disbursement extends StatelessWidget {
+  const Disbursement({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        height: double.infinity,
-        width: double.infinity,
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 112, 23, 184),
-              Color(0xff281537),
-            ],
+       height: double.infinity,
+       width: double.infinity,
+       decoration: const BoxDecoration(
+         gradient: LinearGradient(
+           colors: [
+             Color.fromARGB(255, 112, 23, 184),
+             Color(0xff281537),
+           ]
+         )
+       ),
+       child: Column(
+         children: [
+           const Padding(
+             padding: EdgeInsets.only(top: 400.0),
+           ),
+          const SizedBox(height: 30,),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const DisbursementCheque()));
+            },
+            child: Container(
+              height: 76,
+              width: 357,
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 59, 184, 47), 
+                borderRadius: BorderRadius.circular(30),
+                
+                border: Border.all(color: Colors.white),
+              ),
+              child: const Center(child: Text('For Approval',style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+              ),),),
+            ),
           ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const DisbursementCheque()),
-                );
-              },
-              child: Container(
-                height: 76,
-                width: 357,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.white),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Check Disbursement',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
+          const SizedBox(height: 15,),
+          GestureDetector(
+            onTap: (){
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const TransactionHistory()));
+            },
+            child: Container(
+              height: 76,
+              width: 357,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(color: Colors.white),
               ),
+              child: const Center(child: Text('Transaction History',style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white
+              ),),),
             ),
-            const SizedBox(height: 15),
-            GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const DisbursementCheque()),
-                );
-              },
-              child: Container(
-                height: 76,
-                width: 357,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(color: Colors.white),
-                ),
-                child: const Center(
-                  child: Text(
-                    'Transaction History',
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ),
-            ),
-          ],
-        ),
-      ),
+          ),
+          ]
+       ),
+     ),
     );
   }
 }
