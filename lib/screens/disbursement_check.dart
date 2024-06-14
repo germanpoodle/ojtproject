63c3eeaf4088f665364c9f9b56b15e0309449098
shuakyle <klynejoshua13@gmail.com>
2024-06-14 11:35:58 +0800

    update import disbursement_check

--- lib/screens/disbursement_check.dart
+++ lib/screens/disbursement_check.dart
@@ -1,8 +1,8 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import '/models/transaction.dart';
-import '/widgets/card.dart';
+import '../models/transaction.dart';
+import '../widget/card.dart';
 
 String getCurrentDate() {
   DateTime now = DateTime.now();
